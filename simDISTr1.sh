a48ab84cd5dbc2e0bfffb1afb1cf073e645872da
Plamenna <plamennavenkova@gmail.com>
2018-09-18 00:48:27 +0200

    scrip to run Batch Jobs On Lxplus to run FairShip muonDIS simulation

---
+++ simDISTr1.sh
@@ -0,0 +1,29 @@
+#=========================================================================================================================================
+#Script to submit Job Array with the command: bsub -q 1nh -J "muonDISTr$n[1-89]" " simDISTr1.sh $n" to run FairShip simulation  
+#
+#Run 89 Jobs Arrays , each having size of 100
+#==========================================================================================================================================
+
+echo "Starting script."
+n=$1  
+source /afs/cern.ch/work/p/pvenkova/my_FairShip/FairShipRun/config.sh
+OutputPath=/eos/experiment/ship/user/pvenkova/MuonProduction2018/MuonDISVessel/muonDISmuonsTr1
+File=muonDis_${n}.root
+OutputFile=ship.conical.muonDIS-TGeant4_${n}_${LSB_JOBINDEX}.root
+GeoFile=geofile_full.conical.muonDIS-TGeant4.root
+NJOBS=100
+NTOTAL=50000
+N=$[$NTOTAL/$NJOBS]
+
+if [ -f $OutputPath/$OutputFile ]; then
+       echo "Target exists, nothing to do."
+       exit 0
+else
+	xrdcp $OutputFile/run_simScript.py .
+	python  run_simScript.py --nEvents $N  --firstEvent $[$N*($LSB_JOBINDEX-1)] -f  $OutputPath/$File
+	mv ship.conical.muonDIS-TGeant4.root ship.conical.muonDIS-TGeant4_${n}_${LSB_JOBINDEX}.root
+	ls
+	xrdcp $OutputFile $OutputPath
+	rm run_simScript.py
+	rm $OutputFile
+fi
