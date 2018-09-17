2b40c397c181a08f6fb7b00d5caed58c26ba02fd
Plamenna <plamennavenkova@gmail.com>
2018-09-18 00:24:51 +0200

    script to run Batch Jobs On Lxplus to generate muonDIS with Pythia6

---
+++ makeDISTr1.sh
@@ -0,0 +1,31 @@
+#=========================================================================================================================================
+#                   Script to submit Job Array with the command: bsub -q 1nh -J "muonDISTr1[1-89]" < makeDISTr1.sh  
+#
+#==========================================================================================================================================
+
+echo "Starting script"
+source /afs/cern.ch/work/p/pvenkova/my_FairShip/FairShipRun/config.sh 
+InputPath=/eos/experiment/ship/user/pvenkova/MuonProduction2018/MuonDISVessel/muonDISmuonsTr1
+File=makeMuonDIS.py
+InputFile=muonsNewProductionTr1.root
+OutputPath=/eos/experiment/ship/user/pvenkova/MuonProduction2018/MuonDISVessel/muonDISmuonsTr1
+OutputFile=muonDis_${LSB_JOBINDEX}.root
+nEvent=445 # number of muons in the InputFule
+nIndex=89  # the size of the Job Array , ${LSB_JOBINDEX}
+N=$[$nEvent/$nIndex] 
+
+if [ -f $OutputPath/$OutputFile ]; then
+       	echo "Target exists, nothing to do."
+	exit 0
+else
+	if (($LSB_JOBINDEX == "89" ));then
+
+		python $InputPath/$File -f $InputPath/$InputFile -nPerJobs 5 -nJobs 0  -nDISPerMuon 10000 > out_${LSB_JOBINDEX}
+	else
+		python $InputPath/$File -f $InputPath/$InputFile -nPerJobs 5 -nJobs $LSB_JOBINDEX  -nDISPerMuon 10000 > out_${LSB_JOBINDEX}
+		xrdcp $OutputFile $OutputPath
+		xrdcp out_${LSB_JOBINDEX} $InputPath
+		rm  $OutputFile
+		rm out_${LSB_JOBINDEX}
+	fi
+if
