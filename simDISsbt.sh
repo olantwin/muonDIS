95211bff1e89666bb7ceb2ce61a0223120c3b02b
Plamenna <plamennavenkova@gmail.com>
2018-09-18 16:45:30 +0200

    script to Run Batch Job Array, FAIRSHiP simulation

--- simDISsbt.sh
+++ simDISsbt.sh
@@ -1,17 +1,15 @@
 echo "Starting script."
 n=$1
 source /afs/cern.ch/work/p/pvenkova/my_FairShip/FairShipRun/config.sh
-#OutPath=/eos/experiment/ship/user/pvenkova/MuonProduction2018/MuonDISVessel
-OutPath=/eos/experiment/ship/user/pvenkova/MuonProduction2018/MuonDISVessel/test_deactiveMuonNuclear
+OutPath=/eos/experiment/ship/user/pvenkova/MuonProduction2018/MuonDISVessel
 InputPath=/afs/cern.ch/work/p/pvenkova/Muonprod-2018/muonDISSBT
 File=muonDis_${n}.root
 OutputFile=ship.conical.muonDIS-TGeant4_${n}_${LSB_JOBINDEX}.root
 GeoFile=geofile_full.conical.muonDIS-TGeant4.root
 NJOBS=100
 NTOTAL=9000
-#NTOTAL=1800
 N=$[$NTOTAL/$NJOBS]
-if (($n == "100" ));then
+if (($n == "101" ));then
 	File=muonDis_100.root
 	NJOBS=9
 	NTOTAL=7000
@@ -20,12 +18,8 @@ if (($n == "100" ));then
 	echo "The number of events for 0 muonDIS:", $N
 fi
 
-#if [ -f $InputPath/$OutputFile ]; then
-#       echo "Target exists, nothing to do."
-#       exit 0
-#else
 xrdcp $FAIRSHIP/macro/run_simScript.py .
-#xrdcp /afs/cern.ch/work/p/pvenkova/Muonprod-2018/muonDISSBT/$File . 
+xrdcp /afs/cern.ch/work/p/pvenkova/Muonprod-2018/muonDISSBT/$File . 
 
 python  $FAIRSHIP/macro/run_simScript.py --nEvents $N  --firstEvent $[$N*($LSB_JOBINDEX-1)] -f  $InputPath/$File
 echo "The First event=", $[$N*($LSB_JOBINDEX-1)]
@@ -33,5 +27,3 @@ echo "The N of events=", $N
 mv ship.conical.muonDIS-TGeant4.root ship.conical.muonDIS-TGeant4_${n}_${LSB_JOBINDEX}.root
 ls
 xrdcp $OutputFile $OutPath
-#rm $InputFile
-#fi
